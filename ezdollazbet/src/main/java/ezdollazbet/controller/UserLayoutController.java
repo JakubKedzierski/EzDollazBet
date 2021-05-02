c5378dab395e2892fd7ce1946568738e9ccfe221
Jakub Kedzierski <248915@student.pwr.edu.pl>
2021-05-02 14:41:48 +0200

    lombok add

---
+++ ezdollazbet/src/main/java/ezdollazbet/controller/UserLayoutController.java
@@ -0,0 +1,15 @@
+package ezdollazbet.controller;
+
+import ezdollazbet.EzDollazBetApp;
+import javafx.fxml.FXML;
+
+public class UserLayoutController {
+	
+
+	EzDollazBetApp mainApp;
+	
+	@FXML
+	private void initialize() {}
+	
+
+}
