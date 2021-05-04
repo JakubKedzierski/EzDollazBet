69729b134edb9a15d6f6f04b7a6daa2cdecd0be5
Jakub Kedzierski <248915@student.pwr.edu.pl>
2021-05-04 17:36:29 +0200

    bug fix

--- ezdollazbet/src/main/java/ezdollazbet/view/StartUpController.java
+++ ezdollazbet/src/main/java/ezdollazbet/view/StartUpController.java
@@ -14,44 +14,37 @@ import org.controlsfx.validation.ValidationSupport;
 import org.controlsfx.validation.Validator;
 
 public class StartUpController {
-	
+
 	@Setter
 	EzDollazBetApp mainApp;
-	
+
 	@FXML
 	private Button logInButton;
-	
+
 	@FXML
 	private TextField loginField;
-	
+
 	@FXML
 	private PasswordField passwordField;
 
 	@FXML
-	private void initialize() {  
-	    Platform.runLater(() -> {
-			ValidationSupport validLogin = new ValidationSupport();
-	        validLogin.setErrorDecorationEnabled(false); 
-	        validLogin.registerValidator(loginField, Validator.createEmptyValidator("Pole nie mo¿e byæ puste"));
-	        validLogin.errorDecorationEnabledProperty().bind(loginField.focusedProperty());
-	        logInButton.disableProperty().bind(validLogin.invalidProperty());
-	      });
+	private void initialize() {
 
+		ValidationSupport validLogin = new ValidationSupport();
+		validLogin.setErrorDecorationEnabled(false);
+		validLogin.registerValidator(loginField, Validator.createEmptyValidator("Pole nie mo¿e byæ puste"));
+		validLogin.errorDecorationEnabledProperty().bind(loginField.focusedProperty());
+		logInButton.disableProperty().bind(validLogin.invalidProperty());
 	}
-	
-	
-	@FXML
-	private void logIn(){
-		
 
-        
+	@FXML
+	private void logIn() {
+
 		String login = loginField.getText();
 		String password = passwordField.getText();
-		
-		
-		//if(mainApp.checkLoginEntry(loginField.getText(),passwordField.getText())) {
-			mainApp.initLayout();
-		//}
+
+		mainApp.initLayout();
+
 	}
 
 }
